0421485f4ea8c4d186f56af3e4f69b22d74881da
Mathew V <39657138+Mat-a-tat@users.noreply.github.com>
2024-02-04 22:47:47 -0600

    Region Checker / Basic Weapon Cleaner
    Added the bare bones for the clean weapon function; still need to have it tie directly to other parts of the code before it will be implemented fully.
    
    Added basic formating to a newly created file, as well as the bones to add more formating later if I wish.
    
    Lots of additional safety checks, including cleaning up of input and having user confirmations of performing desturctive actions.

--- project.py
+++ project.py
@@ -5,7 +5,6 @@ import fnmatch
 
 #to do, setup two sheet generation; one for just ratings and the other including comments
 
-
 #authorization
 gc = pygsheets.authorize(service_file=r'C:\Users\mathe\project\MHW_Sheets_Rater\mhw-rating-tracker.json')
 
@@ -37,40 +36,69 @@ monsters = [
 "Blackveil Vaal Hazak", "Velkhana", "Yian Garuga", "Scarred Yian Garuga",
 "Zinogre", "Stygian Zinogre"
 ]
+#to do, replace with full names
 weapons = [
     "GS", "LS", "SNS", "DB", "HAM", "HH", "LANCE", "GL", "SA", "CB", "IG", "LBG", "HBG", "BOW"
 ]
 
-str_cell = str(wks.cell((4,3)))
-print(str_cell[-3:])
 
 def main():
 
     try:
-        s = input("Generate New Sheet / Modify Sheet / See Log\n")
+        opening_question = "\nWelcome! Please type a command below using one of the following keywords. \nGenerate New Sheet / Modify Sheet / Log, open / Quit\nEx: 'gen','mod','log', or 'quit'\n\nTask:"
+        s = input(f"{opening_question}")
+        s = s.replace(' ','')
         answer = s.lower()
-        if answer == 'generate new sheet' or answer == 'gen':
-            make_sheet()
-        if answer == 'modify sheet' or answer == 'mod':
+        if answer == 'generatenewsheet' or answer == 'gen' or answer == 'g':
+            gen_sheet = input("Generating a new sheet can mess up an old one. Still generate a new sheet? (y/n):")
+            if gen_sheet.lower() == 'y': 
+                make_sheet()
+        if answer == 'modifysheet' or answer == 'mod' or answer == 'm':
             mod_sheet()
-        if answer == 'see log' or answer == 'log':
+        if answer == 'seelog' or answer == 'log' or answer == 'l':
             check_log()
+        if answer == 'quit' or answer == 'q' or answer == 'exit':
+            quit()
     except:
-        s = input("Generate New Sheet / Modify Sheet")
+        print("Invalid. Please type a command from below, or the first letter in the command.")
+        s = input(f"{opening_question}")
+
 def make_sheet():
-    df[' '] = monsters
+    # to do, conditional formating
+    # to do, do NOT overwrtite cells that have values in them. 
 
     #update the first sheet with df, starting at cell B2. 
-    wks.update_row(1, weapons, col_offset=0)
-    wks.set_dataframe(df,(1,1))
+    weapon_test_cell = str(wks.cell((1,2)))
+    monster_test_cell = str(wks.cell((2,1)))
 
-    print("Your new worksheet has been created!")
+    if weapon_test_cell[-3:] != "''>" or monster_test_cell[-3:] != "''>":
+        s = input("Theres some values in these cells! Overwrite them? (y/n):")
+        if s.lower() == 'y':
+            df[' '] = monsters
+            wks.update_row(1, weapons, col_offset=0)
+            wks.set_dataframe(df,(1,1))
+            print("Base cells have been populated!")
+        else: 
+            print("No text inside cells modified.")  
+
+    cell_range = 'A1:O66'
+    wks.adjust_column_width(1, end=15, pixel_size=230)
+    #we leave rows with default autoscalling, as setting the column does the bulk of the work.
+    format_info = {
+        "wrapStrategy": "WRAP",
+    }
+    wks.apply_format(cell_range, format_info)
+    #to do, apply alternating colors. bit tricky to get right.
+    #to do, apply additional optional conditianl colors onto cells themselves based on their rating / first few letters.
+    print("Formating Updated!")
+    
+
+
 def mod_sheet():
 
     try:
-        s = input('Example Entry: "ls alatreon s+ skill issue "\n Or, type "log" to see your log. \n Mod:')
-        if s.lower() == 'log':
-            check_log()
+        s = input('Example Entry: "ls alatreon s+ skill issue "\nOr, type "log" to see your log. \nMod:')
+        if s.lower() == 'log': check_log()
         s_split = s.split(' ')
         weapon = s_split[0].upper()
         monster = s_split[1].capitalize()
@@ -80,7 +108,7 @@ def mod_sheet():
         print(f"Weapon: {weapon}")
         print(f"Monster: {monster}")
         print(f"Entry: {text}")
-        
+
         monster_cell = str(wks.find(monster))
         weapon_cell = str(wks.find(weapon))
         
@@ -94,7 +122,6 @@ def mod_sheet():
 
         cell =  weapon_collum + monster_row
         test_cell = str(wks.cell((cell)))
-        # i orignally planned to put some testing in to regrex find partial spellings, but...this does that automaticly. very neat.
 
         if test_cell[-3:] != "''>":
             #to do, regex to get just the text of the cell
@@ -105,18 +132,21 @@ def mod_sheet():
                 mod_sheet()
 
         # Update the cell in the worksheet
-        if len(text) > 0:
+        check = input(f'Cell to be modified: {cell}. Continue? (y/n):')
+    
+        if len(text) > 0 and check.lower != 'n':
             wks.update_value(cell,text)
-            print(f'Cell modified:{cell}')
+            print(f'Cell modified: {cell}')
             print("Update made!")
         else: 
             print("No text changed.")
 
+        main()
+        '''
         s = input("Would you like to make an another entry? (y/n):")
         if s.capitalize() == 'Y':mod_sheet()
-        else: 
-            print("Happy hunting!")
-            quit()
+        '''
+    
 
     except Exception as e:
         print(f"Error: {str(e)}")
@@ -126,89 +156,152 @@ def mod_sheet():
 
     
 def check_log():
-    forest = [
-        'Nargacuga', 'Paolumu', 'Nightshade Paolumu', 'Rathalos', 'Azure Rathalos', 'Rathian',
-        'Pink Rathian', 'Tigrex', 'Brute Tigrex', 'Dodogama', 'Great Girros', 'Great Jagras',
-        'Ebony Odogaron', 'Rajang', 'Zinogre', 'Yian Garuga', 'Scarred Yian Garuga', 'Anjanath',
-        'Fulgur Anjanath', 'Banbaro', 'Brachydios', 'Savage Deviljho', 'Glavenus', 'Namielle',
-        'Ruiner Nergigante', 'Velkhana'
-    ]
+    try:
+        # to do, allow searching via monster 
 
-    wildspire = [
-        'Diablos', 'Black Diablos', 'Legiana', 'Odogaron', 'Ebony Odogaron', 'Rajang',
-        'Tobi-Kadachi', 'Viper Tobi-Kadachi', 'Kulu-Ya-Ku', 'Pukei-Pukei', 'Coral Pukei-Pukei',
-        'Yian Garuga', 'Anjanath', 'Fulgur Anjanath', 'Banbaro', 'Barroth', 'Savage Deviljho',
-        'Glavenus', 'Lunastra', 'Teostra', 'Velkhana'
-    ]
+        #list index out of range errors are likely 
+        world = [
+            "Alatreon", "Ancient Leshen", "Anjanath", "Fulgur Anjanath",
+            "Banbaro", "Barioth", "Frostfang Barioth", "Barroth",
+            "Seething Bazelgeuse", "Behemoth", "Beotodus", "Brachydios",
+            "Raging Brachydios", "Savage Deviljho", "Diablos", "Black Diablos",
+            "Dodogama", "Fatalis", "Glavenus", "Acidic Glavenus",
+            "Great Girros", "Great Jagras", "Jyuratodus", "Kirin",
+            "Kulu-Ya-Ku", "Kulve Taroth", "Kushala Daora", "Lavasioth",
+            "Legiana", "Shrieking Legiana", "Leshen", "Lunastra",
+            "Namielle", "Nargacuga", "Ruiner Nergigante", "Odogaron",
+            "Ebony Odogaron", "Paolumu", "Nightshade Paolumu", 
+            "Pukei-Pukei", "Coral Pukei-Pukei", "Radobaan", "Rajang",
+            "Furious Rajang", "Rathalos", "Azure Rathalos", "Gold Rathalos",
+            "Rathian", "Pink Rathian", "Silver Rathian", "Safi'jiiva",
+            "Shara Ishvalda", "Teostra", "Tigrex", "Brute Tigrex",
+            "Tobi-Kadachi", "Viper Tobi-Kadachi", "Tzitzi-Ya-Ku", "Uragaan",
+            "Blackveil Vaal Hazak", "Velkhana", "Yian Garuga", "Scarred Yian Garuga",
+            "Zinogre", "Stygian Zinogre"
+        ]
+        #having world like this is kind of hacky. I dont like having this list poping up in two places.
+        #However, easier and more consistent than loops and text cleaning below. (this is copoium) 
+        invaders = [
+            'Savage Deviljho', 'Ruiner Nergigante','Rajang', 'Banbaro', 'Fulgur Anjanath', 'Ebony Odogaren',
+        ]
+        #invaders currenty does nothing, but might be useful later.
+        forest = [
+            'Nargacuga', 'Nightshade Paolumu', 'Rathalos', 'Azure Rathalos', 'Rathian', 'Great Jagras',
+            'Tigrex', 'Anjanath', 'Fulgur Anjanath', 'Yian Garuga', 'Scarred Yian Garuga', 'Pukei-Pukei',
+            'Banbaro', 'Brachydios', 'Savage Deviljho', 'Glavenus', 'Ruiner Nergigante', 'Kushala Daora',
+            'Velkhana'
+        ]
 
-    coral = [
-        'Legiana', 'Nargacuga', 'Paolumu', 'Nightshade Paolumu', 'Rathalos', 'Azure Rathalos',
-        'Tzitzi-Ya-Ku', 'Coral Pukei-Pukei', 'Namielle', 'Ruiner Nergigante', 'Velkhana'
-    ]
+        wildspire = [
+            'Diablos', 'Black Diablos', 'Odogaron', 'Ebony Odogaron', 'Rajang', 'Tobi-Kadachi',
+            'Viper Tobi-Kadachi', 'Kulu-Ya-Ku', 'Pink Rathian', 'Pukei-Pukei', 'Banbaro', 'Great Jagras',
+            'Anjanath', 'Fulgur Anjanath', 'Savage Deviljho', 'Lunastra', 'Teostra', 'Glavenus', 'Ruiner Nergigante', 'Velkhana'
+        ]
 
-    rotted = [
-        'Seething Bazelgeuse', 'Nargacuga', 'Rathalos', 'Azure Rathalos', 'Silver Rathalos',
-        'Paolumu', 'Nightshade Paolumu', 'Odogaron', 'Ebony Odogaron', 'Radobaan', 'Banbaro',
-        'Savage Deviljho', 'Glavenus', 'Acidic Glavenus', 'Ruiner Nergigante', 'Velkhana'
-    ]
+        coral = [
+            'Legiana', 'Paolumu', 'Coral Pukei-Pukei', 'Namielle', 'Savage Deviljho','Ruiner Nergigante', 'Rajang', 'Tzitzi-Ya-Ku', 'Kirin', 'Velkhana', 'Zinogre', 
+            'Pink Rathian', 'Banbaro', 'Silver Rathalos', 'Ebony Odogaron', 'Fulgur Anjanath', 'Nargacuga', 'Odogaron'
+        ]
 
-    volcanic = [
-        'Seething Bazelgeuse', 'Rathalos', 'Azure Rathalos', 'Silver Rathalos', 'Dodogama',
-        'Uragaan', 'Brachydios', 'Savage Deviljho', 'Glavenus', 'Acidic Glavenus', 'Ruiner Nergigante', 'Velkhana'
-    ]
+        rotted = [
+            'Brute Tigrex', 'Ebony Odogaron', 'Radobaan','Rajang', 'Savage Deviljho', 'Acidic Glavenus',
+            'Ruiner Nergigante', 'Blackveil Vaal Hazak', 'Velkhana'
+        ]
 
-    tundra = [
-        'Barioth', 'Legiana', 'Shrieking Legiana', 'Nargacuga', 'Rathalos', 'Azure Rathalos',
-        'Silver Rathalos', 'Paolumu', 'Nightshade Paolumu', 'Tigrex', 'Brute Tigrex', 'Odogaron',
-        'Ebony Odogaron', 'Rajang', 'Zinogre', 'Stygian Zinogre', 'Kirin', 'Ruiner Nergigante',
-        'Velkhana'
-    ]
+        volcanic = [
+            'Seething Bazelgeuse', 'Rathalos', 'Azure Rathalos', 'Silver Rathalos', 'Dodogama', 'Uragaan',
+            'Brachydios', 'Savage Deviljho', 'Glavenus', 'Acidic Glavenus','Rajang', 'Ruiner Nergigante', 'Velkhana'
+        ]
 
-    regions = {'forest': forest, 'wildspire': wildspire, 'coral': coral, 'rotted': rotted, 'volcanic': volcanic, 'tundra': tundra}
+        tundra = [
+            'Barioth', 'Shrieking Legiana', 'Nargacuga', 'Tigrex', 'Brute Tigrex', 'Paolumu', 'Nightshade Paolumu',
+            'Odogaron', 'Ebony Odogaron', 'Rajang', 'Zinogre', 'Savage Deviljho','Stygian Zinogre', 'Ruiner Nergigante', 'Velkhana'
+        ]
 
-    s = input("Please type your weapon of choice, followed by hunting region.\nEx: 'ls tundra'")
-    s_split = s.split(' ')
-    weapon = s_split[0].upper()
-    area = s_split[1].lower()
+        regions = {'forest': forest, 'wildspire': wildspire, 'coral': coral, 'rotted': rotted, 'volcanic': volcanic, 'tundra': tundra, 'world':world}
 
-    print(f"Weapon: {weapon}")
-    print(f"Area: {area}")
+        s = input("Please type your weapon followed by 'world' to see the log for that weapon.\n Or type your weapon of choice, followed by hunting region.\nEx: 'sns world'\nEx: 'ls tundra' \nLog Request:")
+        s_split = s.split(' ')
+        weapon = s_split[0].upper()
+        area = s_split[1].lower()
 
-    selected_region = regions.get(area, [])
+        #to do, set this up for more than a demo. 
+        clean_weapon = weapon_cleaner(weapon)
+        print(f"Weapon: {clean_weapon}")
+        print(f"Area: {area}")
+        if area == 'world': print("Were searching the whole world here, so please be patient as this loads.")
 
-    weapon_cell = str(wks.find(weapon))
-    weapon_cell = weapon_cell.split(' ')
-    weapon_column = weapon_cell[1][0]
+        selected_region = regions.get(area, [])
 
-    missing_log = []
+        weapon_cell = str(wks.find(weapon))
+        weapon_cell = weapon_cell.split(' ')
+        weapon_column = weapon_cell[1][0]
 
-    for monster in selected_region:
-        monster_cell = str(wks.find(monster))
-        monster_cell = monster_cell.split(' ')
-        monster_row = monster_cell[1]
-        monster_row = monster_row[1:]
-        cell =  weapon_column + monster_row
-        test_cell = str(wks.cell((cell)))
-        # i orignally planned to put some testing in to regrex find partial spellings, but...this does that automaticly. very neat.
+        missing_log = ''
 
-        if test_cell[-3:] == "''>":
-            missing_log.append(monster)
-    print(missing_log)
+        for monster in selected_region:
+            monster_cell = str(wks.find(monster))
+            monster_cell = monster_cell.split(' ')
+            monster_row = monster_cell[1]
+            monster_row = monster_row[1:]
+            cell =  weapon_column + monster_row
+            test_cell = str(wks.cell((cell)))
 
-    s = input("Would you like to check another log?(y/n):")
-    if s.lower() == 'y':
-        check_log()
-    else:
+
+            if test_cell[-3:] == "''>":
+                missing_log += monster + ', '
+        
+        # todo, chop last comma of and replace with a peroid. 
+        if missing_log == '': print(f"Log for {weapon} complete!")
+        else: print(f"Missing Log for {weapon}: {missing_log}")
+
+        main()
+
+        '''
+        s = input("Would you like to check another log?(y/n):")
+        if s.lower() == 'y':
+            check_log()
+        '''
+        '''
         s = input("Return to main menu? (y/n): ")
         if s.lower() == 'y': main()
         else: print('Happy Hunting!')
+        '''
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        if str(e) == 'list index out of range': print("Double check spellings and spaces of monsters in selected region.")
+        print("Booting you back to startup")
+        main()
+def weapon_cleaner(s):
 
+    #to do, consider using regex for this.
+    #the list are setup with the first value as the return name. Likely not best practice. 
+    ig_list = ['Insect Glaive', 'insectglaive', 'glaive', 'ig', 'insect', 'kinsect']
+    ham_list = ['Hammer', 'hammer', 'ham']
+    sns_list = ['Sword and Shield', 'sword', 'shield', 'sns', 'ss']
+    gs_list = ['Great Sword', 'greatsword', 'great','gs']
+    ls_list = ['Long Sword', 'longsword','long', 'ls']
+    db_list = ['Dual Blades', 'dualblades','blades', 'dual', 'db']
+    sa_list = ['Switch Axe', 'switchaxe', 'switch','axe', 'sa']
+    cb_list = ['Charge Blade', 'chargeblade', 'cb']
+    hh_list = ['Hunting Horn', 'huntinghorn', 'horn', 'hh']
+    lance_list = ['Lance', 'lance', 'ln']
+    gl_list = ['Gunlance', 'gunlance', 'gl']
+    bow_list = ['Bow', 'bow']
+    hbg_list = ['Heavy Bowgun', 'heavybowgun', 'hbg']
+    lbg_list = ['Light Bowgun', 'lightbowgun', 'lbg']
 
+    weapon_nest = [ig_list,ham_list,sns_list,gs_list, ls_list,db_list,sa_list,cb_list,hh_list,lance_list,gl_list,bow_list,hbg_list, lbg_list]
 
+    output = s
+    s = s.lower()
+    s = s.replace(' ','')
+    for weapon_list in weapon_nest:
+        for name_var in weapon_list:
+            if name_var == s: 
+                output = weapon_list[0]
+                break
+    return output
 
-
-
-    
-# to do: list missing monsters, list missing monsters in a GL region, dictonary of monster names / appreviates on a dictonary
 if __name__ == "__main__":
     main()
